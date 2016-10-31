d22f5fb2a819ccdaed23e24e726912edb8384adf
Thomas Balthazar <thomas@balthazar.info>
2016-10-31 11:24:50 +0100

    Minor cleanup

--- lib/resources/tokens_resource.rb
+++ lib/resources/tokens_resource.rb
@@ -5,8 +5,9 @@ module WebmachineHALJSONAPIDemo
     end
 
     def is_authorized?(_authorization_header = nil)
-      @user = User.where(email: params['email']).first
-      @user && @user.password == params['password']
+      # In a real app, I would of course store an
+      # encrypted (bcrypt) version of the password
+      user && user.password == params['password']
     end
 
     def post_is_create?
@@ -14,13 +15,19 @@ module WebmachineHALJSONAPIDemo
     end
 
     def resource_exists?
-      @user
+      user
     end
 
     def process_post
-      @user.generate_token!
-      response.body = %({"token":"#{@user.token}"})
+      user.generate_token!
+      response.body = %({"token":"#{user.token}"})
       true
+    end
+
+    private
+
+    def user
+      @user ||= User.where(email: params['email']).first
     end
   end
 end
